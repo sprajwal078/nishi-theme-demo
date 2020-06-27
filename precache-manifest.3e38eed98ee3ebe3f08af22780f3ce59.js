c454a64cb5831a602ebdf0418bce23fdc5d18634
sprajwal078 <prajwal078@gmail.com>
2020-06-27 05:02:40 +0000

    Update from https://github.com/sprajwal078/nishi-theme-langtang/commit/e07dc712bc8a01ac41847470291198a1de36df05)

--- precache-manifest.3e38eed98ee3ebe3f08af22780f3ce59.js
+++ precache-manifest.3e38eed98ee3ebe3f08af22780f3ce59.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b9478bc054f82c38ea9b9ca32bf3dbd6",
+    "revision": "36845b1bd04450d0b6678856c06de4d0",
     "url": "/nishi-theme-demo/index.html"
   },
   {
-    "revision": "9d82fa40bea41f500504",
-    "url": "/nishi-theme-demo/static/css/main.0cd87866.chunk.css"
+    "revision": "e188a3e099cadeac8956",
+    "url": "/nishi-theme-demo/static/css/main.c1dc75c8.chunk.css"
   },
   {
     "revision": "badf196f87bfac80ceb4",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/nishi-theme-demo/static/js/2.19420ed1.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9d82fa40bea41f500504",
+    "revision": "e188a3e099cadeac8956",
     "url": "/nishi-theme-demo/static/js/main.3afcae48.chunk.js"
   },
   {
