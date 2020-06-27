af0e92662de0220187bd7a155a2e1fc2de42ddda
sprajwal078 <prajwal078@gmail.com>
2020-06-27 04:46:35 +0000

    Update from https://github.com/sprajwal078/nishi-theme-langtang/commit/c35ad216d5a03bb17bc3690679d4df0466c48618)

---
+++ precache-manifest.d00170022071e0893c17637617dd673a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15a98a2b7589a2dbd8904fd1d97d79b0",
+    "url": "/nishi-theme-demo/index.html"
+  },
+  {
+    "revision": "7c51a682367ccb728100",
+    "url": "/nishi-theme-demo/static/css/main.0cd87866.chunk.css"
+  },
+  {
+    "revision": "badf196f87bfac80ceb4",
+    "url": "/nishi-theme-demo/static/js/2.19420ed1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/nishi-theme-demo/static/js/2.19420ed1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c51a682367ccb728100",
+    "url": "/nishi-theme-demo/static/js/main.74d061d5.chunk.js"
+  },
+  {
+    "revision": "ca302b1a594807a3019c",
+    "url": "/nishi-theme-demo/static/js/runtime-main.92eb5616.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e1a2e67e01831257885e02ad7abbc9ea.js"
+  "/nishi-theme-demo/precache-manifest.d00170022071e0893c17637617dd673a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nishi-theme-demo/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
